2e513ba0145db7438056f246748c45a3fcc93cf9
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-10 18:07:18 +0800

    added networking capabilities

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import server from '../networking';
 
 function Home() {
     const Universal = useSelector(state => state.universal)
+    const [healthCheck, setHealthCheck] = useState('Contacting...')
+
+    useEffect(() => {
+        checkHealth()
+    })
+
+    const checkHealth = () => {
+        // setHealthCheck('Checking...')
+        server.get("/misc/health")
+        .then(response => {
+            if (response.status == 200) {
+                setHealthCheck(response.data)
+            }
+        })
+        .catch(err => {
+            setHealthCheck('Error: ' + err.message)
+        })
+    }
 
     return <div>
         <p>Home</p>
         <p>{Universal.systemName}, {Universal.systemVersion}</p>
+        <p>Backend health check: {healthCheck}</p>
+        <button onClick={checkHealth}>Check Health</button>
     </div>
 }
 
