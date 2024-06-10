b744b6559e41e2b4631e5d4c9203881e4e463fb9
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-10 17:48:38 +0800

    created universal slice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const Universal = useSelector(state => state.universal)
+
+    return <div>
+        <p>Home</p>
+        <p>{Universal.systemName}, {Universal.systemVersion}</p>
+    </div>
 }
 
 export default Home
