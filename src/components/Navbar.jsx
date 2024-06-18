5a2d68615d5fad5449d6a7405dec3e3e1e7e7e0f
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-18 22:46:16 +0800

    added short stack font, hamburger icon and avatar icon
    using chakra UI Icons library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import { Box, Center, Container, Heading, Text } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { Avatar, Box, Center, Container, GenericAvatarIcon, HStack, Heading, Spacer, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Navbar() {
     return (
-        <Container bgGradient={"linear(to-br, #ff86d6, #ffa14a)"} dropShadow={"5px solid"} maxWidth={"100%"} padding={"10px"} rounded={"10px"} mb={"20px"}>
-            <Text color={"white"} fontWeight={"bold"}>MakanMatch</Text>
+        <Container bgGradient={"linear(to-br, #ff86d6, #ffa14a)"} dropShadow={"5px solid"} maxWidth={"90%"} rounded={"10px"} mb={"20px"}>
+            <HStack padding={"5px"}>
+                <HamburgerIcon color={"white"} />
+                <Spacer />
+                <Text color={"white"} fontFamily={"Short Stack"} fontWeight={"bold"} fontSize={"large"}>MakanMatch</Text>
+                <Spacer />
+                <Avatar size={"sm"} />
+            </HStack>
         </Container>
     )
 }
