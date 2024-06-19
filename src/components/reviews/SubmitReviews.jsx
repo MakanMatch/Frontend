85fbc694136a998e65487f0ce66dead96c5dd933
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-19 22:52:57 +0800

    completed upload images for review feature in submit review component

--- src/components/reviews/SubmitReviews.jsx
+++ src/components/reviews/SubmitReviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import StarRating from './StarRatings';
+import { submitReviews } from '../../slices/ReviewsState';
 import { Button, Box, Input, Flex, Avatar, Text, Center, Container, Image, Textarea, Spacer } from '@chakra-ui/react';
 import { useDispatch } from 'react-redux';
 import { useDisclosure } from '@chakra-ui/react'
@@ -24,7 +25,29 @@ function SubmitReviews() {
   const dispatch = useDispatch()
   const [foodRating, setFoodRating] = useState(0);
   const [hygieneRating, setHygieneRating] = useState(0)
+  const [comments, setComments] = useState('')
+  const [images, setImages] = useState([]);
+  const [previews, setPreviews] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const handleImageChange = (event) => {
+    const newFiles = Array.from(event.target.files);
+    setImages(prevImages => [...prevImages, ...newFiles]);
+    const newPreviews = newFiles.map(file => URL.createObjectURL(file));
+    setPreviews(prevPreviews => [...prevPreviews, ...newPreviews]);
+  };
+  const handleSubmit = () => {
+    const reviewData = {
+      sender: "Susie Jones",
+      receiver: "Jamie Oliver",
+      foodRating,
+      hygieneRating,
+      comments,
+      images
+    };
+    dispatch(submitReviews(reviewData));
+    onClose();
+  };
+
   return (
     <Box>
       <Button onClick={onOpen} variant={"MMPrimary"}> Submit Reviews </Button>
@@ -48,12 +71,12 @@ function SubmitReviews() {
                 <Flex>
                   <Flex direction='column'>
                     <Text mt='8px' mb='8px'>Food Rating</Text>
-                    <StarRating maxStars={5} foodRating={foodRating} setFoodRating={setFoodRating} />
+                    <StarRating maxStars={5} rating={foodRating} onChange={setFoodRating} />
                   </Flex>
                   <Spacer/>
                   <Flex direction='column'>
                     <Text mt='8px' mb='8px'>Hygiene Rating</Text>
-                    <StarRating maxStars={5} hygieneRating={hygieneRating} setHygieneRating={setHygieneRating} />
+                    <StarRating maxStars={5} rating={hygieneRating} onChange={setHygieneRating} />
                   </Flex>
                 </Flex>
               </Box>
@@ -62,14 +85,31 @@ function SubmitReviews() {
                 placeholder='Write something to the host...'
                 size='sm'
                 resize='none'
+                value={comments}
+                onChange={(e) => setComments(e.target.value)}
               />
+              <Text mt='16px' mb='8px'>Upload Images</Text>
+              <Input pt="4px" type='file' accept='image/*' multiple onChange={handleImageChange} />
+              <Flex mt={4} wrap="wrap">
+                {previews.map((preview, index) => (
+                  <Image
+                    key={index}
+                    src={preview}
+                    alt={`preview ${index}`}
+                    boxSize="100px"
+                    objectFit="cover"
+                    m={1}
+                    borderRadius="md"
+                  />
+                ))}
+              </Flex> 
             </FormControl>
           </ModalBody>
           <ModalFooter>
             <Button colorScheme='red' borderRadius='10px' mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant='MMPrimary' type='submit'>Submit</Button>
+            <Button variant='MMPrimary' type='submit' onClick={handleSubmit}>Submit</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
