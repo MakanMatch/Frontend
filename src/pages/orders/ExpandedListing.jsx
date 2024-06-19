0255b6eeb68af16dba1b2e0f6058d551fbb0a4dd
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-20 00:19:00 +0800

    trying to get image upload to work
    form data is an empty object in the request to the backend for some reason

--- src/pages/orders/ExpandedListing.jsx
+++ src/pages/orders/ExpandedListing.jsx
@@ -1,5 +1,5 @@
 import { PlusSquareIcon, SmallAddIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Center, Container, EditableTextarea, Flex, Grid, GridItem, HStack, Heading, Image, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Spacer, Spinner, Text, Textarea, VStack, useToast } from '@chakra-ui/react'
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Center, Container, EditableTextarea, Flex, Grid, GridItem, HStack, Heading, Image, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Spacer, Spinner, Text, Textarea, VStack, useToast, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, StatUpArrow, Input } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import ReservationSettingsCard from '../../components/orders/ReservationSettingsCard'
@@ -27,6 +27,8 @@ function ExpandedListing() {
         published: null
     })
     const [loading, setLoading] = useState(true)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [file, setFile] = useState(null);
 
     const showToast = (title, description, duration = 5000, isClosable = true, status = 'info', icon = null) => {
         if (!["success", "warning", "error", "info"].includes(status)) {
@@ -61,25 +63,29 @@ function ExpandedListing() {
     }
 
     useEffect(() => {
-        server.get("/listingDetails")
-        .then(response => {
-            if (response.status == 200) {
-                console.log(response.data)
-                setListingData(processData(response.data))
-                setLoading(false)
-                return
-            } else {
-                showToast("Error", "Failed to retrieve listing details", 5000, true, "error")
-                console.log(response.data)
-                return
-            }
-        })
-        .catch(err => {
-            showToast("Error", "Failed to retrieve listing details", 5000, true, "error")
-            console.log(err)
-            return
-        })
+        fetchListingDetails()
     }, [])
+
+    const fetchListingDetails = () => {
+        server.get("/listingDetails")
+            .then(response => {
+                if (response.status == 200) {
+                    console.log(response.data)
+                    setListingData(processData(response.data))
+                    setLoading(false)
+                    return
+                } else {
+                    showToast("Error", "Failed to retrieve listing details", 5000, true, "error")
+                    console.log(response.data)
+                    return
+                }
+            })
+            .catch(err => {
+                showToast("Error", "Failed to retrieve listing details", 5000, true, "error")
+                console.log(err)
+                return
+            })
+    }
 
     function Statistic({ value, description }) {
         return (
@@ -94,56 +100,121 @@ function ExpandedListing() {
         return (<Spinner />)
     }
 
+    const handleFileSubmission = (e) => {
+        if (e.target.files.length > 0) {
+            setFile(e.target.files[0])
+            console.log("file set")
+        }
+    }
+
+    const uploadImage = (e) => {
+        e.preventDefault();
+        if (file == null) {
+            showToast("Error", "No file selected", 2500, true, "error")
+            return
+        }
+
+        const formData = new FormData()
+        console.log(file)
+        formData.append('file', file)
+        formData.append('fileName', file.name)
+        formData.append('listingID', listingData.listingID)
+        const config = {
+            'content-type': 'multipart/form-data'
+        }
+
+        server.post("/uploadListingImage", formData, config)
+            .then(res => {
+                if (res.status == 200 && res.data.startsWith("SUCCESS")) {
+                    showToast("Success", "Image uploaded successfully", 2500, true, "success")
+                    onClose()
+
+                    return
+                } else {
+                    showToast("Error", "Failed to upload image", 5000, true, "error")
+                    console.log(res.data)
+                    return
+                }
+            })
+            .catch(err => {
+                showToast("Error", "Failed to upload image", 5000, true, "error")
+                console.log(err)
+                return
+            })
+    }
+
     return (
-        <Grid
-            h={"100%"}
-            templateColumns='repeat(3, 1fr)'
-            gap={4}
-            p={"10px"}
-        >
-            <GridItem colSpan={3} mb={"20px"}>
-                <VStack alignItems={"flex-start"}>
-                    <Text>{listingData.datetime}</Text>
-                    <Heading>{listingData.title}</Heading>
+        <>
+            <Grid
+                h={"100%"}
+                templateColumns='repeat(3, 1fr)'
+                gap={4}
+                p={"10px"}
+            >
+                <GridItem colSpan={3} mb={"20px"}>
+                    <VStack alignItems={"flex-start"}>
+                        <Text>{listingData.datetime}</Text>
+                        <Heading>{listingData.title}</Heading>
 
-                    <HStack spacing={"10px"} overflowX={"auto"} height={"250px"}>
-                        {listingData.images.map(imgName => {
-                            return (
-                                <Image maxH={"100%"} objectFit={"cover"} display={"block"} rounded={"10px"} src={imgBackendURL(imgName)} />
-                            )
-                        })}
-                        <Center h={"100%"} aspectRatio={"1"} bg={"gray.300"} textAlign={"center"} onClick={showComingSoon}>
-                            <SmallAddIcon boxSize={"10"} />
-                        </Center>
-                    </HStack>
-                </VStack>
-            </GridItem>
-            <GridItem colSpan={2}>
-                <VStack alignItems={"flex-start"} spacing={{ base: "10px", md: "20px", lg: "30px" }}>
-                    <VStack alignItems={"flex-start"} width={"100%"}>
-                        <Text fontWeight={"bold"} mb={"10px"}>Description</Text>
-                        <Textarea placeholder='Describe your dish here' value={listingData.longDescription} />
-                    </VStack>
-
-                    <Spacer />
-
-                    <VStack alignItems={"flex-start"} width={"100%"}>
-                        <Heading size={"md"}>Listing Statistics</Heading>
-
-                        <HStack spacing={"30px"} wrap={"wrap"}>
-                            <Statistic value={"2/5"} description={"Reservations"} />
-                            <Statistic value={"1000"} description={"Impressions"} />
-                            <Statistic value={"45%"} description={"Click-Through Rate"} />
-                            <Statistic value={"$7.00"} description={"Revenue"} />
+                        <HStack spacing={"10px"} overflowX={"auto"} height={"250px"}>
+                            {listingData.images.map(imgName => {
+                                return (
+                                    <Image maxH={"100%"} objectFit={"cover"} display={"block"} rounded={"10px"} src={imgBackendURL(imgName)} />
+                                )
+                            })}
+                            <Center h={"100%"} aspectRatio={"1"} bg={"gray.300"} textAlign={"center"} onClick={onOpen}>
+                                <SmallAddIcon boxSize={"10"} />
+                            </Center>
                         </HStack>
                     </VStack>
-                </VStack>
-            </GridItem>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <VStack alignItems={"flex-start"} spacing={{ base: "10px", md: "20px", lg: "30px" }}>
+                        <VStack alignItems={"flex-start"} width={"100%"}>
+                            <Text fontWeight={"bold"} mb={"10px"}>Description</Text>
+                            <Textarea placeholder='Describe your dish here' value={listingData.longDescription} />
+                        </VStack>
 
-            <GridItem colSpan={1}>
-                <ReservationSettingsCard listingPublished={listingPublished} setListingPublished={setListingPublished} pricePerPortion={pricePerPortion} setPricePerPortion={setPricePerPortion} />
-            </GridItem>
-        </Grid>
+                        <Spacer />
+
+                        <VStack alignItems={"flex-start"} width={"100%"}>
+                            <Heading size={"md"}>Listing Statistics</Heading>
+
+                            <HStack spacing={"30px"} wrap={"wrap"}>
+                                <Statistic value={"2/5"} description={"Reservations"} />
+                                <Statistic value={"1000"} description={"Impressions"} />
+                                <Statistic value={"45%"} description={"Click-Through Rate"} />
+                                <Statistic value={"$7.00"} description={"Revenue"} />
+                            </HStack>
+                        </VStack>
+                    </VStack>
+                </GridItem>
+
+                <GridItem colSpan={1}>
+                    <ReservationSettingsCard listingPublished={listingPublished} setListingPublished={setListingPublished} pricePerPortion={pricePerPortion} setPricePerPortion={setPricePerPortion} />
+                </GridItem>
+            </Grid>
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Upload New Image</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <VStack textAlign={"left"} spacing={"25px"} p={"10px"}>
+                            <Text width={"100%"}>Upload a new image for your dish!</Text>
+                            <Text>Please be reminded that images must not contain explicit content; the images you upload should be real-life pictures of your dish only.</Text>
+                            <Input type={"file"} p={"10px"} h={"20%"} onChange={handleFileSubmission} />
+                        </VStack>
+                    </ModalBody>
+                    <ModalFooter>
+                        <HStack spacing={"20px"}>
+                            <Button onClick={onClose}>Close</Button>
+                            <Button colorScheme={"blue"} onClick={uploadImage}>Upload</Button>
+                        </HStack>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 
