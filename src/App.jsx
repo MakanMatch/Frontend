9a812048b20630244fee8610d013cd0783a88146
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-20 23:25:01 +0800

    created individual page for reveiws

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
-import Reviews from './components/reviews/SubmitReviews'
-
+import Reviews from './pages/reviews/Reviews'
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <>
-            <Navbar />
-            <Home />
-            <Reviews />
-        </>
-    )
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/reviews" element={<Reviews />} />
+          </Routes>
+        </BrowserRouter>
+      );
 }
 
 export default App
