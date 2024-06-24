d14e63d517457eda9adc8b2a2a2704f7227107d1
Nic <3xpect1916@gmail.com>
2024-06-24 21:22:31 +0800

    added image to users

--- src/components/chat/ChatBubble.jsx
+++ src/components/chat/ChatBubble.jsx
@@ -1,37 +1,69 @@
 import React from "react";
-import { Box, Text, Flex } from "@chakra-ui/react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Box, Text, Flex, Image } from "@chakra-ui/react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 
-function ChatBubble({ message, timestamp, isSender }) {
+function ChatBubble({ message, timestamp, isSender, photoUrl }) {
   return (
-    <Flex
-      justifyContent={isSender ? 'flex-end' : 'flex-start'}
-      marginBottom={2}
-    >
-      {!isSender && (
-        <ChevronLeftIcon color="gray.500" boxSize={6} marginRight={-2} />
-      )}
-      <Box
-        bg={isSender ? 'green.500' : 'gray.200'}
-        color={isSender ? 'white' : 'black'}
-        borderRadius="lg"
-        p={3}
-        maxWidth="70%"
-      >
-        <Text>{message}</Text>
-        {timestamp && (
-          <Text fontSize="xs" color={isSender ? 'gray.300' : 'gray.500'} marginTop={1} textAlign="right">
-            {timestamp}
-          </Text>
+    <Box position="relative" marginY={2} maxW="70%" alignSelf={isSender ? 'flex-end' : 'flex-start'}>
+      <Flex alignItems="center">
+        {!isSender && (
+          <Image
+            src={photoUrl}
+            alt="Profile"
+            boxSize="40px"
+            borderRadius="full"
+            marginRight={3}
+            marginTop={"-25px"}
+          />
         )}
-      </Box>
-      {isSender && (
-        <ChevronRightIcon color="green.500" boxSize={6} marginLeft={-2} />
-      )}
-    </Flex>
+        <Box position="relative">
+          {isSender && (
+            <ChevronLeftIcon 
+              color="blue.500" 
+              boxSize={8} 
+              position="absolute" 
+              top="25%" 
+              transform="translateY(-50%)" 
+              right="-20px"
+            />
+          )}
+          {!isSender && (
+            <ChevronLeftIcon 
+              color="black.200" 
+              boxSize={8} 
+              position="absolute" 
+              top="25%" 
+              transform="translateY(-50%)" 
+              left="-20px"
+            />
+          )}
+          <Box
+            bg={isSender ? 'blue.500' : 'gray.200'}
+            color={isSender ? 'white' : 'black'}
+            borderRadius="lg"
+            p={3}
+          >
+            <Text>{message}</Text>
+            {timestamp && (
+              <Text fontSize="xs" color={isSender ? 'gray.300' : 'gray.500'} marginTop={1} textAlign="right">
+                {timestamp}
+              </Text>
+            )}
+          </Box>
+        </Box>
+        {isSender && (
+          <Image
+            src={photoUrl}
+            alt="Profile"
+            boxSize="40px"
+            borderRadius="full"
+            marginLeft={3}
+            marginTop={"-25px"}
+          />
+        )}
+      </Flex>
+    </Box>
   );
 }
 
 export default ChatBubble;
-
-
