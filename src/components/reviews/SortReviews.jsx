3a775403f5824d6df7053ba68abaa3b0a75c51f6
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-28 10:01:26 +0800

    removed image for now

--- src/components/reviews/SortReviews.jsx
+++ src/components/reviews/SortReviews.jsx
@@ -121,11 +121,6 @@ function SortReviews() {
                                     alt='Review Image'
                                 />
                             )} */}
-                            <Image
-                                objectFit='cover'
-                                src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                                alt='Chakra UI'
-                            />
                             <CardFooter
                                 justify='space-between'
                                 flexWrap='wrap'
