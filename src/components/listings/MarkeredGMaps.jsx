c403fd3297ae5d77d16a1e4328a2c5df3a591dc7
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-07-08 17:31:12 +0800

    Formatted doc and handled mapRef promise rejection

--- src/components/listings/MarkeredGMaps.jsx
+++ src/components/listings/MarkeredGMaps.jsx
@@ -7,83 +7,110 @@ import { useToast } from "@chakra-ui/react";
 import configureShowToast from "../../components/showToast";
 import server from "../../networking";
 
-const MarkeredGMaps = ({ addresses, listings, userID, isSmallerThan1095, getImageLink }) => {
-  const mapRef = useRef(null);
-  const toast = useToast();
-  const showToast = configureShowToast(toast);
-  const navigate = useNavigate();
+const MarkeredGMaps = ({
+    addresses,
+    listings,
+    userID,
+    isSmallerThan1095,
+    getImageLink,
+}) => {
+    const toast = useToast();
+    const showToast = configureShowToast(toast);
+    const navigate = useNavigate();
+    const mapRef = useRef(null);
 
-  useEffect(() => {
-    if (addresses.length > 0) {
-      const fetchCoordinates = async (address) => {
-        const encodedAddress = encodeURIComponent(String(address));
-        const apiKey = import.meta.env.VITE_GMAPS_API_KEY;
-        const url = `https://maps.googleapis.com/maps/api/geocode/json?address="${encodedAddress}"&key=${apiKey}`;
-        try {
-          const response = await server.get(url);
-          return response.data.results[0].geometry.location;
-        } catch (error) {
-          showToast("An error occured", "Failed to generate maps coordinates", "error", 3000);
-          return null;
-        }
-      };
+    useEffect(() => {
+        const fetchCoordinates = async (address) => {
+            const encodedAddress = encodeURIComponent(String(address));
+            const apiKey = import.meta.env.VITE_GMAPS_API_KEY;
+            const url = `https://maps.googleapis.com/maps/api/geocode/json?address="${encodedAddress}"&key=${apiKey}`;
+            try {
+                const response = await server.get(url);
+                return response.data.results[0].geometry.location;
+            } catch (error) {
+                showToast(
+                    "An error occured",
+                    "Failed to generate maps coordinates",
+                    "error",
+                    3000
+                );
+                return null;
+            }
+        };
 
-      const initializeMap = async (validCoordinates) => {
-        const loader = new Loader({
-          apiKey: import.meta.env.VITE_GMAPS_API_KEY,
-          version: "weekly",
-          libraries: ["places"],
-        });
-        const { Map } = await loader.importLibrary("maps");
-        const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker");
+        const InitializeMap = async (validCoordinates) => {
+            const loader = new Loader({
+                apiKey: import.meta.env.VITE_GMAPS_API_KEY,
+                version: "weekly",
+                libraries: ["places"],
+            });
+            const { Map } = await loader.importLibrary("maps");
+            const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker");
 
-        if (validCoordinates.length === 0) {
-          new Map(mapRef.current, {
-            center: { lat: 1.3521, lng: 103.8198 },
-            zoom: 11,
-            mapId: import.meta.env.VITE_GMAPS_MAPID,
-            mapTypeControl: false,
-            streetViewControl: false,
-          });
-        } else {
-          const map = new Map(mapRef.current, {
-            center: { lat: 1.3621, lng: 103.8198 },
-            zoom: 11,
-            mapId: import.meta.env.VITE_GMAPS_MAPID,
-            mapTypeControl: false,
-            streetViewControl: false,
-          });
-          validCoordinates.forEach(({ lat, lng }, index) => {
-            const images = listings[index].images.map((imageName) =>
-                getImageLink(listings[index].listingID, imageName)
-            )
-            const marker = new AdvancedMarkerElement({
-              position: { lat, lng },
-              map: map,
-            })
-            marker.addListener("click", () => {
-                const listing = listings[index];
-                navigate(`/targetListing?latitude=${lat}&longitude=${lng}&listingID=${listing.listingID}&userID=${userID}&images=${encodeURIComponent(JSON.stringify(images))}&title=${listing.title}&shortDescription=${listing.shortDescription}&approxAddress=${listing.approxAddress}&portionPrice=${listing.portionPrice}&totalSlots=${listing.totalSlots}`)
-              });
-          });
-        }
-      };
+            if (mapRef.current) {
+                const map = new Map(mapRef.current, {
+                    center: { lat: 1.3621, lng: 103.8198 },
+                    zoom: 11,
+                    mapId: import.meta.env.VITE_GMAPS_MAPID,
+                    mapTypeControl: false,
+                    streetViewControl: false,
+                });
 
-      Promise.all(addresses.map(fetchCoordinates))
+                if (addresses.length === 0 || validCoordinates.length === 0) {
+                    console.log("No valid coordinates found");
+                    return map;
+                } else {
+                    validCoordinates.forEach(({ lat, lng }, index) => {
+                        const images = listings[index].images.map((imageName) =>
+                            getImageLink(listings[index].listingID, imageName)
+                        );
+                        const marker = new AdvancedMarkerElement({
+                            position: { lat, lng },
+                            map: map,
+                        });
+                        marker.addListener("click", () => {
+                            const listing = listings[index];
+                            navigate(
+                                `/targetListing?latitude=${lat}&longitude=${lng}&listingID=${
+                                    listing.listingID
+                                }&userID=${userID}&images=${encodeURIComponent(
+                                    JSON.stringify(images)
+                                )}&title=${listing.title}&shortDescription=${
+                                    listing.shortDescription
+                                }&approxAddress=${
+                                    listing.approxAddress
+                                }&portionPrice=${
+                                    listing.portionPrice
+                                }&totalSlots=${listing.totalSlots}`
+                            );
+                        });
+                    });
+                }
+            } else return;
+        };
+
+        Promise.all(addresses.map(fetchCoordinates))
         .then((coordinatesList) => {
-          const validCoordinates = coordinatesList.filter(({ lat, lng }) => lat >= -90 && lat <= 90 && lng >= -180 && lng <= 180);
-          initializeMap(validCoordinates);
+            const validCoordinates = coordinatesList.filter(
+                ({ lat, lng }) =>
+                    lat >= -90 && lat <= 90 && lng >= -180 && lng <= 180
+            );
+            InitializeMap(validCoordinates);
         })
         .catch((error) => {
-          console.error("Error fetching address coordinates:", error);
+            console.error("Error fetching address coordinates:", error);
         });
-    }
-}, [addresses]);
+    }, [addresses]);
 
     return (
         <div
-        ref={mapRef}
-        style={{ height: isSmallerThan1095 ? "45vh" : "83vh", width: "100%", borderRadius: "10px"}}/>
+            ref={mapRef}
+            style={{
+                height: isSmallerThan1095 ? "45vh" : "83vh",
+                width: "100%",
+                borderRadius: "10px",
+            }}
+        />
     );
 };
 
