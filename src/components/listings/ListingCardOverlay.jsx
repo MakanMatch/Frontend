ce8976738de9b4a116c59a8073a7458257461412
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-07-13 02:18:06 +0800

    Updated Reviews.jsx with Lincoln's latest code on GitHub to test authState flow. Updated useLocation state to just hostID, as userID is no longer needed after authState

--- src/components/listings/ListingCardOverlay.jsx
+++ src/components/listings/ListingCardOverlay.jsx
@@ -230,7 +230,7 @@ function ListingCardOverlay({ listingID, userID, hostID, images, title, shortDes
                                 />
                             </Box>
                         </Box>
-                        <Link to={'/reviews'} state={{ userID, hostID }}>
+                        <Link to={'/reviews'} state={{ hostID }}>
                             <Text mt={2} mb={-4} textAlign="left" color="blue" fontSize={"13px"} textDecoration={"underline"} cursor={"pointer"}>View Host Reviews</Text>
                         </Link>
                     </Stack>
