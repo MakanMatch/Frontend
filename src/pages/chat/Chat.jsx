41d1cec1bfc6d0811fde57b2021df3af9bad217b
Nicholas Chew <3xpect1916@gmail.com>
2024-07-19 12:56:26 +0800

    added the user type to send to the backend

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -64,7 +64,7 @@ function ChatUi() {
 
 		ws.current.onopen = () => {
 			console.log("Connected to WebSocket server");
-			ws.current.send(JSON.stringify({ action: "connect", userID: user.userID }))
+			ws.current.send(JSON.stringify({ action: "connect", userID: user.userID, userType: user.userType }))
 		};
 
 		ws.current.onmessage = async (event) => {
