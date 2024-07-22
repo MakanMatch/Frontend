d01a2c38afb4f362b4ae6f79d07bbddd992aa773
Lincoln Lim <lincolnlim267@gmail.com>
2024-07-22 12:38:32 +0800

    created alert before performing deletion

--- src/components/reviews/actions/DeleteReviewButton.jsx
+++ src/components/reviews/actions/DeleteReviewButton.jsx
@@ -1,10 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Box, Button, useToast } from '@chakra-ui/react';
 import { BiTrash } from 'react-icons/bi';
 import configureShowToast from '../../showToast';
 import server from '../../../networking';
 import { useDispatch, useSelector } from 'react-redux';
 import { reloadAuthToken } from '../../../slices/AuthState';
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+} from '@chakra-ui/react';
 
 function DeleteReviewButton({
     reviewID,
@@ -15,6 +23,9 @@ function DeleteReviewButton({
     const showToast = configureShowToast(toast);
     const { user, authToken } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
+    const [isOpen, setIsOpen] = useState(false);
+    const onClose = () => setIsOpen(false);
+    const cancelRef = useRef();
 
     const handleDelete = async () => {
         try {
@@ -47,12 +58,39 @@ function DeleteReviewButton({
     return (
         <Box>
             <Button
-                onClick={handleDelete}
+                onClick={() => setIsOpen(true)}
                 colorScheme="red"
                 ml={2}
                 size="md">
                 <BiTrash />
             </Button>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                isCentered
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Delete Review
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure you want to delete this review? This action cannot be undone.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme="red" onClick={handleDelete} ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </Box>
     );
 }
