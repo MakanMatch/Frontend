b00a75ea3261b8f6bfbc453523d532668c9feb7b
JunHam <joonjunhan@gmail.com>
2024-07-28 16:55:31 +0800

    Added user management

--- src/components/identity/UserManagementCard.jsx
+++ src/components/identity/UserManagementCard.jsx
@@ -2,14 +2,15 @@ import { Avatar, Box, HStack, Heading, Text } from '@chakra-ui/react'
 import { BsThreeDotsVertical } from 'react-icons/bs';
 
 function UserManagementCard({ username, email, userType }) {
+	console.log("Received props: ", username, email, userType);
 	return (
         <HStack display="flex" justifyContent={"space-between"}>
             <Box display="flex" alignItems="center" width={"40%"} ml={3}>
                 <Avatar />
                 <Box ml={3}>
-                    <Heading size='sm' minWidth={"290px"} maxWidth={"290px"} overflow={"hidden"} textOverflow={"ellipsis"} whiteSpace={'nowrap'} textAlign={"left"}>
+                    <Text size='sm' minWidth={"290px"} maxWidth={"290px"} overflow={"hidden"} textOverflow={"ellipsis"} whiteSpace={'nowrap'} textAlign={"left"}>
                         {username}
-                    </Heading>
+                    </Text>
                 </Box>
             </Box>
 
--- src/pages/identity/Admin/UserManagement.jsx
+++ src/pages/identity/Admin/UserManagement.jsx
@@ -1,22 +1,70 @@
-import { Heading, Card, CardHeader, CardBody, Stack, HStack, StackDivider, Box, Text, Avatar  } from "@chakra-ui/react";
-import { BsThreeDotsVertical } from 'react-icons/bs';
+import { Heading, Card, CardHeader, CardBody, Stack, StackDivider, HStack, Box, Text } from "@chakra-ui/react";
 import UserManagementCard from "../../../components/identity/UserManagementCard";
+import server from ".././../../networking"
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 function UserManagement() {
+    const [users, setUsers] = useState([])
+    const fetchAllUsers = async () => {
+        const response = await server.get('/cdn/fetchAllUsers')
+        if (response.status === 200) {
+            console.log(response.data)
+            setUsers(response.data)
+        }
+    }
+
+    useEffect(() => {
+        fetchAllUsers()
+        console.log("Ran")
+    }, [])
+    
     return (
         <>
             <Heading size='lg' textAlign={"center"} mt={10} mb={5}>User Management</Heading>
-            <Card>
+            <Card p={2}>
+                <CardHeader>
+                    <HStack>
+                        <Box display="flex" alignItems="center" width={"40%"} ml={3}>
+                            <Box ml={3}>
+                                <Heading size='sm' minWidth={"290px"} maxWidth={"290px"} overflow={"hidden"} textOverflow={"ellipsis"} whiteSpace={'nowrap'} textAlign={"left"} ml={-3}>
+                                    User
+                                </Heading>
+                            </Box>
+                        </Box>
+
+                        <Box width={"37%"}>
+                            <Heading size='sm' minWidth={"290px"} maxWidth={"290px"} overflow={"hidden"} textOverflow={"ellipsis"} whiteSpace={'nowrap'} textAlign={"left"}>
+                                Email
+                            </Heading>
+                        </Box>
+                        
+                        <Box width={"20%"}>
+                            <Heading size='sm' textAlign={"left"}>
+                                Role
+                            </Heading>
+                        </Box>
+                        
+                        <Box width={"3%"}>
+                            <Text size='sm'>
+                            </Text>
+                        </Box>
+                    </HStack>
+                </CardHeader>
                 <CardBody>
                     <Stack divider={<StackDivider />} spacing='4'>
-                        <UserManagementCard username={"Username1"} email={"username1@gmail.com"} userType={"Host"}/>
-                        <UserManagementCard username={"Username2"} email={"userName2@gmail.com.sgsgsgsggssgsgsggsgsgsgsgsgsgggsgsggs"} userType={"Guest"} />
-                        <UserManagementCard username={"Username390234570892347085972308574352908"} email={"pmtnssssssssssssssssss@gmail.com"} userType={"Host"} />
-                        <UserManagementCard username={"Username3"} email={"XXXXXXXXXXXXXXXXXXX"} userType={"Host"} />
-                        <UserManagementCard username={"Username4"} email={"XXXXXXXXXXXXXXXXXXX"} userType={"Guest"} />  
-                        <UserManagementCard username={"Username5"} email={"XXXXXXXXXXXXXXXXXXX"} userType={"Guest"} />
-                        <UserManagementCard username={"Username6"} email={"XXXXXXXXXXXXXXXXXXX"} userType={"Guest"} />
-                        <UserManagementCard username={"Username7"} email={"XXXXXXXXXXXXXXXXXXX"} userType={"Guest"} />
+                    {users.length > 0 ? (
+                        users.map((user) => (
+                            <UserManagementCard
+                                key={user.userID}
+                                username={user.username}
+                                email={user.email}
+                                userType={user.userType}
+                            />
+                        ))
+                    ) : (
+                        <Text>No users found.</Text>
+                    )}
                     </Stack>
                 </CardBody>
             </Card>
