bb4a9b5a57aec304c2ed0f8ab5fa3363b87c4998
Lincoln Lim <lincolnlim267@gmail.com>
2024-07-30 12:18:16 +0800

    Changed UI for guest management

--- src/components/orders/GuestManagement.jsx
+++ src/components/orders/GuestManagement.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import server from '../../networking'
-import { Spinner, Text, Box, Button } from '@chakra-ui/react'
+import { Spinner, Text, Box, Button, Avatar, IconButton, Flex, Spacer } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { useToast } from '@chakra-ui/react'
 import { useSelector, useDispatch } from 'react-redux'
 import { reloadAuthToken } from '../../slices/AuthState'
 import configureShowToast from '../../components/showToast'
+import { FaCommentDots } from "react-icons/fa";
 
 function GuestManagement({
     hostID,
@@ -93,24 +94,54 @@ function GuestManagement({
         <>
             {guestsList.length > 0 && (
                 <>
-                    <Text fontWeight="bold" fontSize="large" display="flex" mb={4} >Guests</Text>
-                    <Text textAlign="left" color="grey" fontSize="large" display="flex" >When guests arrive, check that they have paid and click "Paid & Present" below to let us know.</Text>
+                    <Text fontWeight="bold" fontSize="large" display="flex" mb={10}>Guests</Text>
+                    <Text textAlign="left" color="grey" fontSize="large" display="flex" mb={10}>When guests arrive, check that they have paid and click "Paid & Present" to let us know.</Text>
                 </>
             )}
             {guestsList.length > 0 ? (
                 guestsList.map((guest) => (
-                    <Box key={guest.userID} borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4}>
-                        <Text fontWeight="bold" fontSize="lg">{guest.fname} {guest.lname}</Text>
-                        <Text>Portions: {guest.Reservation.portions}</Text>
-                        <Text>Total Price: ${guest.Reservation.totalPrice}</Text>
-                        <Text>Paid & Present: {guest.Reservation.paidAndPresent ? 'Yes' : 'No'}</Text>
-                        <Button
-                            mt={2}
-                            colorScheme="green"
-                            onClick={() => handlePaidAndPresent({ referenceNum: guest.Reservation.referenceNum, listingID: listingID })}
-                        >
-                            Mark as Paid & Present
-                        </Button>
+                    <Box
+                        key={guest.userID}
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="space-between"
+                        borderWidth="1px"
+                        borderRadius="lg"
+                        overflow="hidden"
+                        p={4}
+                        mb={4}
+                    >
+                        <Box display="flex" alignItems="center">
+                            <Avatar name={guest.username} size="lg" mr={4} />
+                            <Box>
+                                <Flex justify="space-between" width="100%">
+                                    <Box>
+                                        <Text fontWeight="bold" fontSize="lg" textAlign="right">{guest.fname} {guest.lname}</Text>
+                                    </Box>
+                                </Flex>
+                                <Flex mt={2} wrap="wrap" justify="space-between" width="100%" gap={3}>
+                                    <Text color="grey" fontSize="large">Total portion: {guest.Reservation.portions}</Text>
+                                    <Spacer />
+                                    <Text color="grey" fontSize="large">Total price: ${guest.Reservation.totalPrice}</Text>
+                                </Flex>
+                            </Box>
+                        </Box>
+                        <Box display="flex" alignItems="center">
+                            <IconButton
+                                icon={<FaCommentDots />}
+                                aria-label="Message Guest"
+                                mr={4}
+                                variant="ghost"
+                                colorScheme="blackAlpha"
+                                size="lg"
+                            />
+                            <Button
+                                colorScheme="purple"
+                                onClick={() => handlePaidAndPresent({ referenceNum: guest.Reservation.referenceNum, listingID: listingID })}
+                            >
+                                Paid & Present
+                            </Button>
+                        </Box>
                     </Box>
                 ))
             ) : (
