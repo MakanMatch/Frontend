9446eddb4be389c9eff06de8b98eaf031ddfffe1
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-01 02:38:29 +0800

    Enhanced UI

--- src/pages/customerSupport/MakanBot.jsx
+++ src/pages/customerSupport/MakanBot.jsx
@@ -1,6 +1,5 @@
-import { Box, Card, CardBody, Heading, Text, Image, CardFooter, FormControl, Input, useMediaQuery, useToast } from '@chakra-ui/react';
+import { Box, Card, CardBody, Heading, Text, Image, CardFooter, FormControl, Input, useMediaQuery, useToast, Button } from '@chakra-ui/react';
 import Logo from '../../assets/Logo.png';
-import { AddIcon } from '@chakra-ui/icons';
 import { FaPaperPlane } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { reloadAuthToken } from '../../slices/AuthState';
@@ -9,13 +8,36 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 function MakanBot() {
-    const [isSmallerThan845] = useMediaQuery("(max-width: 845px)");
+    const [isSmallerThan880] = useMediaQuery("(max-width: 880px)");
     const [promptResult, setPromptResult] = useState("Having doubts? MakanBot is here to help.");
 
     const toast = useToast();
     const dispatch = useDispatch();
 
     const authToken = useSelector((state) => state.auth.authToken);
+
+    const suggestedPrompts = [
+        {
+            displayMessage: "Reservations",
+            prompt: "As a guest, how could I reserve a meal slot?"
+        },
+        {
+            displayMessage: "Payments",
+            prompt: "How do I make payments for my meal?"
+        },
+        {
+            displayMessage: "Cancellations",
+            prompt: "How do I cancel a reservation?"
+        },
+        {
+            displayMessage: "Host's reviews",
+            prompt: "How do I view a host's profile and reviews?"
+        },
+        {
+            displayMessage: "Hosting a meal",
+            prompt: "As a host, how could I host a meal for the community?"
+        }
+    ]
 
     function displayToast(title, description, status, duration, isClosable) {
         toast.closeAll();
@@ -26,6 +48,10 @@ function MakanBot() {
             duration: duration,
             isClosable: isClosable
         });
+    }
+
+    const autofillSuggestedPrompt = (prompt) => {
+        document.getElementById("promptInput").value = prompt;
     }
 
     const handleSubmitPrompt = async () => {
@@ -91,41 +117,72 @@ function MakanBot() {
             display="flex"
             justifyContent="space-between"
             height={"80vh"}
+            minW={"202px"}
         >
-            {!isSmallerThan845 && (
-                <Card width="24%">
+            {!isSmallerThan880 && (
+                <Card width="29%">
                     <CardBody>
+                        <Heading fontSize="22px" mb={5} textAlign={"left"} fontWeight={"bold"} color="grey">Suggested topics: </Heading>
+                        {suggestedPrompts.map((prompt, index) => (
+                            <motion.div
+                                key={index}
+                                initial={{ opacity: 0, y: 10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <Box
+                                    backgroundColor={"#F1F4FF"}
+                                    padding={3}
+                                    borderRadius={"3xl"}
+                                    cursor={"pointer"}
+                                    mb={4}
+                                    sx={{
+                                        transition: 'background-color 0.2s ease-in-out',
+                                        _hover: {
+                                            backgroundColor: "#E2E8F0",
+                                            transition: 'background-color 0.2s ease-in-out'
+                                        }
+                                    }}
+                                >
+                                    <Text textAlign={"center"} ml={2} color="#4A5568" maxHeight="36px" overflow="hidden" textOverflow={"ellipsis"} onClick={() => autofillSuggestedPrompt(prompt.prompt)}>{prompt.displayMessage}</Text>
+                                </Box>
+                            </motion.div>
+                        ))}
+                    </CardBody>
+                    <CardFooter display="flex" flexDir={"column"} mt={-4}>
                         <Box display="flex" justifyContent={"center"} mt={-3}>
                             <Image src={Logo} height="100px" maxWidth="100px" />
                         </Box>
                         <Text fontSize={"25px"} textAlign="center" mt={-2} bgGradient={"linear(to-br, #ff86d6, #ffa14a)"} bgClip={"text"} fontFamily={"Short Stack"}>MakanBot</Text>
-                        <Box
-                            backgroundColor={"#F1F4FF"}
-                            padding={3}
-                            borderRadius={"3xl"}
-                            cursor={"pointer"}
-                            mt={2}
-                            sx={{
-                                transition: 'background-color 0.2s ease-in-out',
-                                _hover: {
-                                    backgroundColor: "#E2E8F0",
-                                    transition: 'background-color 0.2s ease-in-out'
-                                }
-                            }}
-                        >
-                            <Box display="flex">
-                                <AddIcon color={"#4A5568"} mt={1} ml={2}/>
-                                <Text textAlign={"left"} ml={2} color="#4A5568">New chat</Text>
-                            </Box>
-                        </Box>
-                    </CardBody>
+                    </CardFooter>
                 </Card>
             )}
-            <Card width={isSmallerThan845 ? "100%" : "74%"}>
-                <CardBody>
-                    <Heading as="h1" size="lg" textAlign="center">MakanBot</Heading>
-                    <Text textAlign="center" fontSize="xl">MakanBot is here to help you with any questions you may have!</Text>
-                    <Box backgroundColor={"#F1F4FF"} width="80%" height="70%" margin="auto" borderRadius={"2xl"} mt={5} display="flex" justifyContent={"center"}>
+            <Card width={isSmallerThan880 ? "100%" : "69%"}>
+            <CardBody>
+                <Heading as="h1" size="lg" textAlign="center" mb={5}>MakanBot</Heading>
+                <Box 
+                    position="relative"
+                    height="100%" 
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    overflow="hidden"
+                >
+                    <Box
+                        backgroundColor="#F1F4FF"
+                        width={isSmallerThan880 ? "100%" : "80%"}
+                        height="auto"
+                        maxHeight="308px"
+                        borderRadius="2xl"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        position="absolute"
+                        top="50%"
+                        left="50%"
+                        transform="translate(-50%, -50%)"
+                        overflow="hidden"
+                    >
                         <motion.div
                             key={promptResult}
                             initial={{ opacity: 0, y: 10 }}
@@ -133,10 +190,11 @@ function MakanBot() {
                             transition={{ duration: 0.5 }}
                             style={{ margin: 'auto' }}
                         >
-                            <Text>{promptResult}</Text>
+                            <Text padding={5}>{promptResult}</Text>
                         </motion.div>
                     </Box>
-                </CardBody>
+                </Box>
+            </CardBody>
                 <CardFooter>
                     <Box display="flex" justifyContent={"space-between"} width="100%">
                         <Box width="100%">
