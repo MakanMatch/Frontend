39ff267b65c4401104c54df9077102327cee07d4
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-08-07 16:14:55 +0800

    added alert dialog and button in listing management to delete listing

--- src/pages/orders/ExpandedListingHost.jsx
+++ src/pages/orders/ExpandedListingHost.jsx
@@ -374,7 +374,7 @@ function ExpandedListingHost() {
                         </GridItem>
 
                         <GridItem colSpan={{ base: 3, md: 1 }} mt={3}>
-                            <ReservationSettingsCard listingPublished={listingPublished} paymentImage={hostPaymentImage} togglePublished={togglePublished} setEditListing={setEditListing} pricePerPortion={pricePerPortion} guestSlots={guestSlots} minGuests={listingData.guests.map(g => g.Reservation.portions).reduce((t, n) => t + n, 0)} handleSettingsChange={handleSettingsChange} />
+                            <ReservationSettingsCard listingID={listingID} listingPublished={listingPublished} paymentImage={hostPaymentImage} togglePublished={togglePublished} setEditListing={setEditListing} pricePerPortion={pricePerPortion} guestSlots={guestSlots} minGuests={listingData.guests.map(g => g.Reservation.portions).reduce((t, n) => t + n, 0)} handleSettingsChange={handleSettingsChange} />
                         </GridItem>
 
                         <UploadNewImageModal isOpen={isOpen} handleClose={handleClose} handleFileSubmission={handleFileSubmission} isUploading={isUploading} uploadImage={uploadImage} />
